d4618b38236fee58be685bde15f55f8268fec5e3
Moosa <mawad@student.42abudhabi.ae>
2024-08-28 17:19:42 +0400

    Added a full implementation of ex01 but not in class form yet

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -2,6 +2,7 @@
 #include <stack>
 #include <iostream>
 #include <sstream>
+#include <cstdlib>
 
 bool	is_op(std::string& str)
 {
@@ -31,7 +32,7 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 	std::string 			token_list = argv[1];
-	std::stack<std::string>	stack;
+	std::stack<int>			stack;
 	std::istringstream 		stream(token_list);
 	std::string				token;
 
@@ -39,6 +40,32 @@ int main(int argc, char *argv[])
 		std::cout << "token : " << token << "." << std::endl;
 		if (!is_integer(token) && !is_op(token))
 			return (std::cerr << "Invalid entry option -> " << token << std::endl, 1);
+		if (is_integer(token))
+			stack.push(atoi(token.c_str()));
+		if (is_op(token))
+		{
+			int result;
+			int store;
+
+			store = stack.top();
+			stack.pop();
+			if (token == "*")
+				result = stack.top() * store;
+			else if (token == "/")
+				result = stack.top() / store;
+			else if (token == "+")
+				result = stack.top() + store;
+			else if (token == "-")
+				result = stack.top() - store;
+			stack.pop();
+			stack.push(result);
+		}
 	}
+	if (stack.size() > 1)
+	{
+		std::cerr << "Invalid RPN operation" << std::endl;
+		return (2);
+	}
+	std::cout << "Result is -> " << stack.top() << std::endl;
 	return (0);
 }
